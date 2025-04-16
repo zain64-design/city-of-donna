0dd7cb82e435988dcad75c7d8b73c57d86a453af
zain64-design <zainyzain64@gmail.com>
2025-04-16 05:14:57 +0500

    FancyBox name bug fixed

--- src/components/Common/Fancybox.jsx
+++ src/components/Common/Fancybox.jsx
@@ -3,7 +3,7 @@ import React, { useRef, useEffect } from "react";
 import { Fancybox as NativeFancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
-function Fancybox(props) {
+function FancyBox(props) {
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -23,4 +23,4 @@ function Fancybox(props) {
   return <div ref={containerRef}>{props.children}</div>;
 }
 
-export default Fancybox;
+export default FancyBox;
